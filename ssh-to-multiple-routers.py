8ae784ce8371cee129d72571d833dd971d911fb9
Roger Perkin <roger@rogerperkin.co.uk>
2020-09-07 22:14:11 +0100

    added extra print chars

---
+++ ssh-to-multiple-routers.py
@@ -0,0 +1,24 @@
+# SSH to Multiple Devices from devices file
+from netmiko import ConnectHandler
+
+with open('devices.txt') as routers:
+    for IP in routers:
+        Router = {
+            'device_type': 'cisco_ios',
+            'ip': IP,
+            'username': 'roger',
+            'password': 'cisco'
+        }
+
+        net_connect = ConnectHandler(**Router)
+
+        print ('Connecting to ' + IP)
+        print('-'*79)
+        output = net_connect.send_command('sh ip int brief')
+        print(output)
+        print()
+        print('-'*79)
+
+
+
+
