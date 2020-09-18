68aab71aa426ce61f13dc5b0615c2597a5795487
Roger Perkin <roger@rogerperkin.co.uk>
2020-09-18 21:08:44 +0100

    updated devices

--- ping-trace.py
+++ ping-trace.py
@@ -8,7 +8,7 @@ import time
 CURRENT_DIR = os.getcwd()
 START_TIME = time.time()
 MAX_THREADS = 5
-FILENAME = 'ips.txt'
+FILENAME = 'devices.txt'
 PING_COMMAND = 'ping -q -c 3 -W 1'
 TRACEROUTE_COMMAND = 'traceroute'
 
