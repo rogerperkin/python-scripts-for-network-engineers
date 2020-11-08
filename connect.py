8a68b08bb7e4c3597237bea5d512f64d0246f64b
Roger <roger@rogerperkin.co.uk>
2020-11-08 11:47:32 -0500

    updated code

--- connect.py
+++ connect.py
@@ -1,15 +1,22 @@
-## Simple Python Script to connect to Cisco Router using Netmiko 
+from netmiko import ConnectHandler
 
-import netmiko 
+#First create the device object using a dictionary
+CSR = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.1.220',
+    'username': 'roger',
+    'password': 'cisco'
+}
 
-connection = netmiko.ConnectHandler (ip='192.168.1.220', device_type='cisco_ios', username='roger', password='cisco')
+# Next establish the SSH connection
+net_connect = ConnectHandler(**CSR)
 
-result = connection.send_command('sh ip int brief')
+# Then send the command and print the output
+output = net_connect.send_command('show ip int brief')
+print (output)
 
-print(result)
-
+# Finally close the connection
 connection.disconnect()
-
 
 
 
